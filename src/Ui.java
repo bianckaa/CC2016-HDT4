226fee3ac4e4580d87617b87b8ca4459a4cb013a
bianckaa <bianckaraxon@gmail.com>
2025-02-27 15:25:44 -0600

    rename

--- src/Ui.java
+++ src/Ui.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class UI {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ReaderDocument readerDocument = new ReaderDocument();
